45ccb4fc78c6140fff08af16793713a5f83380e9
azerose <woojjang8671@naver.com>
2023-01-14 16:00:49 +0900

    FEATURE: 로그인 기능구현

--- renderer/components/units/login/loginContainer.tsx
+++ renderer/components/units/login/loginContainer.tsx
@@ -2,29 +2,27 @@ import { useForm } from "react-hook-form";
 import LoginPresenter from "./loginPresenter";
 import { IFormData } from "./loginType";
 import useMoveToPage from "../../commons/hooks/useMoveToPage";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { Auth } from "../../commons/firebase/firebase";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { LoginYup } from "./loginShema";
-import { app } from "electron";
+import { Login } from "../../commons/firebase/firebase";
+import { errorMsg, success } from "../../commons/Modal";
 
 const LoginContainer = () => {
-  app;
-  console.log(app);
-
   const { onClickMoveToPage } = useMoveToPage();
 
   const { register, formState, handleSubmit } = useForm<IFormData>({
     resolver: yupResolver(LoginYup),
   });
 
-  const onClickLogin = async (data: IFormData) => {
-    const { user } = await signInWithEmailAndPassword(
-      Auth,
-      data.email,
-      data.password
-    );
-    // const { stsTokenManager, uid } = user;
+  const onClickLogin = (data: IFormData) => {
+    try {
+      Login(data.email, data.password);
+      success("로그인에 성공하셨습니다");
+    } catch (error) {
+      if (error instanceof Error) {
+        errorMsg(error.message);
+      }
+    }
   };
 
   return (
